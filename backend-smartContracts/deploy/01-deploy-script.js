a52cb60c85e2aacf5b6305e660453399b93707d3
Bhimgouda <satvikp508@gmail.com>
2023-06-03 10:53:13 +0530

    added deploy script

--- backend-smartContracts/deploy/01-deploy-script.js
+++ backend-smartContracts/deploy/01-deploy-script.js
@@ -7,6 +7,24 @@ const {verify} = require("../utils/verify")
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy, log, get} = deployments
     const {deployer} = await getNamedAccounts()
-    const {name: networkName} = network;
 
+    const requiredApprovals = 2
+    const owners = ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"]
+    const args = [requiredApprovals, owners]
+
+    const MultiSigWallet = await deploy("MultiSigWallet", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1
+    })
+
+    // Verify the deployment
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+    log("Verifying...")
+    await verify(MultiSigWallet.address, args);
+    }
+    log("--------------------------------------")
 }
+
+module.exports.tags = ["all", "main"];
